01f5a5eb6216d4408267156321ea71e44ab30857
Daniel Jaffe <danielj@othentic.xyz>
2024-08-12 22:49:13 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,42 +1,32 @@
 #!/usr/bin/env bash
 
-# Function to show help
 show_help() {
     echo "Usage: run.sh [OPTIONS]"
     echo ""
     echo "Options:"
     echo "  --destroy    Destroy the environment"
-    echo "  --local      Run docker-compose with local configuration"
-    echo "  --holesky    Run docker-compose with Holesky configuration"
+    echo "  --local      Run blade network with local configuration"
+    echo "  --holesky    Run blade network with Holesky L1"
     echo "  --help       Display this help message"
 }
 
-# Function to destroy the environment
 destroy_environment() {
     echo "Destroying the environment..."
 
     cd docker/local
     
-    # Stop and remove all containers defined in the docker-compose file
     docker-compose down
     
-    # Remove all images defined in the docker-compose file
     docker-compose rm -f -v
     
-    # Remove all unused images
     docker image prune -af
     
-    # Remove all unused volumes
     docker volume prune -f
     
-    # Remove specific volumes if needed
     docker volume rm $(docker volume ls -qf dangling=true)
     
-    # Optionally, remove network if it exists and is not needed
     docker network rm blade-docker-network
     
-    # Optionally, remove local files if needed (adjust paths as necessary)
-    # For example, remove data directories
     rm -rf docker/local/services/blockscout-db-data
     rm -rf docker/local/services/logs
     rm -rf docker/local/services/redis-data
@@ -45,7 +35,6 @@ destroy_environment() {
     echo "Docker environment destroyed successfully."
 }
 
-# Parse command line arguments
 case "$1" in
     --destroy)
         destroy_environment
